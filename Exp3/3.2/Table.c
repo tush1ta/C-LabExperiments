73fddd61782430eb48f28d8c231fd51bf3a85c20
tush1ta <tushitasharma833@gmail.com>
2025-10-31 01:36:51 +0530

    Add files via upload

---
+++ Exp3/3.2/Table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter a number to print its multiplication table: ");
+    scanf("%d", &n);
+
+    printf("\nMultiplication Table of %d:\n", n);
+
+    for (i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
