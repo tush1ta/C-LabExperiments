33fe70e3c93a4536de0d586346dc846536b5c0ed
tush1ta <tushitasharma833@gmail.com>
2025-10-31 00:24:50 +0530

    Add files via upload

---
+++ Exp1/address.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+printf("Name: Tushita Sharma\n");
+printf("House number: 27\n");
+printf("Street: Bidholi\n");
+printf("City: Dehradun\n");
+printf("State: Uttrakhand\n");
+printf("Pin Code: 248001\n");
+return 0;
+}
